dc786b7647a09d02d69dd0700e44d28f81b1f469
lider <vadim4ik9@gmail.com>
2022-03-26 14:16:57 +0300

    initial commit

---
+++ SwiftUI+Alamofire/SwiftUI_AlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_AlamofireApp.swift
+//  SwiftUI+Alamofire
+//
+//  Created by Vadim on 26.03.22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_AlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
